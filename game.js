40868339d16031439fa3c3c64907eb2526cf01d9
Сергей <zhukovskysv1610@gmail.com>
2018-07-08 13:26:28 +0300

    class Vector

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
 'use strict';
+
+class Vector {
+	constructor (x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	plus (vector) {
+		if (!(vector instanceof Vector)) {
+			throw new Error ('Vector.plus: Прибавляемый вектор должен быть объектом Vector');
+		}
+		return new Vector (this.x + vector.x, this.y + vector.y);
+	}
+	
+	times (n) {
+		return new Vector (this.x * n, this.y * n);
+	}
+}
