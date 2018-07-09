571c3ace3bf3ae7868bf8fd944d9d9594cd9107e
Сергей <zhukovskysv1610@gmail.com>
2018-07-09 22:28:39 +0300

    class Actor

--- game.js
+++ game.js
@@ -17,3 +17,52 @@ class Vector {
         return new Vector(this.x * n, this.y * n);
     }
 }
+
+class Actor {
+    constructor(pos = new Vector(), size = new Vector(1, 1), speed = new Vector()) {
+        if (pos instanceof Vector && size instanceof Vector && speed instanceof Vector) {
+            this.pos = pos;
+            this.size = size;
+            this.speed = speed;
+        } else {
+            throw new Error('Actor.constructor: все аргументы должны быть объектами Vector');
+        }
+
+    }
+
+    get left() {
+        return this.pos.x;
+    }
+
+    get right() {
+        return this.pos.x + this.size.x;
+    }
+
+    get top() {
+        return this.pos.y;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y;
+    }
+
+    get type() {
+        return 'actor';
+    }
+
+    act() {
+    }
+
+    isIntersect(actor) {
+        if (!(actor instanceof (Actor)) || actor === undefined) {
+            throw new Error('Actor.isIntersect: аргумент должен быть объектом Actor')
+        } else if (actor === this) {
+            return false;
+        } else {
+			return !(this.right <= actor.left ||
+            this.left >= actor.right ||
+            this.bottom <= actor.top ||
+            this.top >= actor.bottom);
+		}
+    }
+}
